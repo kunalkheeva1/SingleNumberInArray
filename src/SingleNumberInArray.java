0b5fff438c9ec1b3a5258ee7edd163a44f55a87c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-14 02:20:01 -0700

    created a method to check the unique element in the array.

--- src/SingleNumberInArray.java
+++ src/SingleNumberInArray.java
@@ -1,3 +1,16 @@
 public class SingleNumberInArray {
-    
+
+    public static int singleNumberInArray(int [] arr){
+        int xor =0;
+
+        for(int i=0; i<arr.length; i++){
+            xor ^=arr[i];
+        }return xor;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
