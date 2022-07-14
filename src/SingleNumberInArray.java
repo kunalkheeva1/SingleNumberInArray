37faef1e4d76c11a5f9b8a0b5230efb44c5316fb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-14 02:31:01 -0700

    added comments for better understanding

--- src/SingleNumberInArray.java
+++ src/SingleNumberInArray.java
@@ -1,9 +1,15 @@
 public class SingleNumberInArray {
 
+    //check unique element in the given array
     public static int singleNumberInArray(int [] arr){
+
+        //take xor as 0
         int xor =0;
 
         for(int i=0; i<arr.length; i++){
+            //start with 0's xor as the xor of 0 will return the same element
+            //afterwards the xor of same elements gives zero and then at the end returns
+            //the remaining unique element
             xor ^=arr[i];
         }return xor;
     }
